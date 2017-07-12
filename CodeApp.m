46b279eeec3a8dea2767080eed4095f57421cf5b
Gagnon <gagnon_alex@videotron.ca>
2017-07-12 00:43:27 -0400

    version final du code, titre et tweaking completé

--- CodeApp.m
+++ CodeApp.m
@@ -28,7 +28,7 @@ figure
 pr = roots(p_initial)
 % p = [.99*pr(1) .99*pr(2) 1/pr(3) 1/pr(4) 1/pr(5) 1/pr(6) ] %meilleur graphe de filtre
 p = [.95*pr(1) .95*pr(2) 1/pr(3) 1/pr(4) 1/pr(5) 1/pr(6) ] %meilleur logique zplane
-p_stable = poly(p)*10 ;
+p_stable = poly(p) ;
 
  figure('name','filtre stabiliser inverse')
 zplane(z_initial,p_stable);
@@ -42,51 +42,99 @@ y_stable = filter(z_initial,p_stable,son_bruite);
 %% composition RIF
 
 module_filtre = abs(freqz(z_initial,p_stable,512/2));
-figure('name','allo loloolo')
-stem(module_filtre);
-title('module filtre ')
+
+FE = 1:fe;
+module_filtre_hz = abs(freqz(z_initial,p_stable));
+freqz(z_initial,p_stable);
+title('module filtre RII inversé stable')
+figure('name','gain du filtre inverse stable + valeur gains')
+subplot(4,1,1);
+plot(module_filtre_hz);
+
+
+% x=[64;128;192]; 
+% 
+%  ystop=[2;2;2];
+%  x = [x x];
+%  nloop = size(x,1);
+%  for ii = 1:nloop
+%     plot(x(ii,:),[0 ystop(ii,1)])
+%  end
+% 
+% hold off;
+
+title('module filtre RII ');xlabel('Fréquence'); ylabel('Amplitude')
 
 RIF4 = RIF_creator(module_filtre,4);
 RIF8 = RIF_creator(module_filtre,8);
 RIF16 = RIF_creator(module_filtre,16);
 
-figure
-subplot(3,1,1)
-plot(RIF4);
-subplot(3,1,2)
-plot(RIF8);
-subplot(3,1,3)
-plot(RIF16);
 
-figure
-subplot(3,1,1)
-filtre_RIF4 = fftshift(ifft(RIF4));
-plot(abs(filtre_RIF4));
-
-subplot(3,1,2)
-filtre_RIF8 = fftshift(ifft(RIF8));
-plot(abs(filtre_RIF8));
-
-subplot(3,1,3)
-filtre_RIF16 = fftshift(ifft(RIF16));
-plot(abs(filtre_RIF16));
-
-figure('name', 'filtrer avec inverse stable');
-subplot(4,1,1)
-plot (son_bruite);
-title('son bruite')
 subplot(4,1,2)
+hold on;
+stem(RIF4);
+x=[64;128;192;256]; 
+
+ ystop=[2;2;2;2];
+ x = [x x];
+ nloop = size(x,1);
+ for ii = 1:nloop
+    plot(x(ii,:),[0 ystop(ii,1)])
+ end
+
+title('gain avec spectre séparé en 4 bandes ');xlabel('Échantillons'); ylabel('Amplitude')
+subplot(4,1,3)
+stem(RIF8);
+
+
+
+title('gain avec spectre séparé en 8 bandes ');xlabel('Échantillons'); ylabel('Amplitude')
+subplot(4,1,4)
+stem(RIF16);
+title('gain avec spectre séparé en 16 bandes ');xlabel('Échantillons'); ylabel('Amplitude')
+
+figure
+freqz(ifftshift(ifft(RIF4)));
+title('gain avec pectre séparé en 4 bandes');
+figure
+freqz(ifftshift(ifft(RIF8)));
+title('gain avec pectre séparé en 8 bandes');
+figure
+freqz(ifftshift(ifft(RIF16)));
+title('gain avec pectre séparé en 16 bandes');
+figure('name','réponse impulsionelle')
+subplot(3,1,1)
+
+ filtre_RIF4 = ifftshift(ifft(RIF4));
+plot(abs(filtre_RIF4));
+title('réponse impulsionelle du filtre à 4 bandes');xlabel('Échantillons'); ylabel('Amplitude')
+
+subplot(3,1,2)
+filtre_RIF8 = ifftshift(ifft(RIF8));
+plot(abs(filtre_RIF8));
+title('réponse impulsionelle du filtre à 8 bandes');xlabel('Échantillons'); ylabel('Amplitude')
+
+subplot(3,1,3)
+filtre_RIF16 = ifftshift(ifft(RIF16));
+plot(abs(filtre_RIF16));
+title('réponse impulsionelle du filtre à 16 bandes');xlabel('Échantillons'); ylabel('Amplitude')
+
+% figure('name', 'filtrer avec inverse stable');
+% subplot(4,1,1)
+% plot (son_bruite);
+% title('son bruite') ;xlabel('Échantillons'); ylabel('Amplitude')
+% subplot(4,1,2)
 
 convRIF4 = conv(son_bruite,abs(filtre_RIF4))
 filtRIF4 = filter(abs(filtre_RIF4),1,son_bruite);
 
-plot(filtRIF4);
-title('filtre impulsion RIF4 passer dans filter');
-
-
-subplot(4,1,3)
-plot(y_stable);
-title('filtre impulsion avec filtre inverse s tabiliser');
+% plot(filtRIF4);
+% title('filtre impulsion RIF4 passer dans filter');
+% 
+% 
+% subplot(4,1,3)
+% plot(y_stable);
+% title('filtre impulsion avec filtre inverse s tabiliser');
 
 
 
@@ -100,7 +148,7 @@ fft_fin = fft(bruit_fin, fe);
 
 % figure('name', 'fft debut')
 % stem(abs(fft_debut)); 
-% 
+% axis([0 8000 0 inf])
 % figure('name', 'fft fin')
 % stem(abs(fft_fin));
 
@@ -125,28 +173,28 @@ b_CoupeB = poly(zero_coupeB);
 
 %% test son
 
-subplot(4,1,1);
-plot(son_bruite,'r');
-title('Son Bruite');
-
-s_debruitrer = filter(b_CoupeB,a_CoupeB,son_bruite);
-subplot(4,1,2);
-plot(s_debruitrer,'b');
-title('Son debruiter par RII');
-
-s_debruiterRIF4 = filter(abs(filtre_RIF4),1,s_debruitrer);
-s_debruiterRIF8 = filter(abs(filtre_RIF8),1,s_debruitrer);
-s_debruiterRIF16 = filter(abs(filtre_RIF16),1,s_debruitrer);
-subplot(4,1,3);
-plot(s_debruiterRIF16,'g');
-title('Son debruiter par RII + RIF4');
-
-hold on;
-subplot(4,1,4);
-plot(son_bruite,'r');
-plot(s_debruiterRIF16,'g');
-plot(s_debruitrer,'b');
-hold off
+% subplot(4,1,1);
+% plot(son_bruite,'r');
+% title('Son Bruite');
+% 
+% s_debruitrer = filter(b_CoupeB,a_CoupeB,son_bruite);
+% subplot(4,1,2);
+% plot(s_debruitrer,'b');
+% title('Son debruiter par RII');
+% 
+% s_debruiterRIF4 = filter(abs(filtre_RIF4),1,s_debruitrer);
+% s_debruiterRIF8 = filter(abs(filtre_RIF8),1,s_debruitrer);
+% s_debruiterRIF16 = filter(abs(filtre_RIF16),1,s_debruitrer);
+% subplot(4,1,3);
+% plot(s_debruiterRIF16,'g');
+% title('Son debruiter par RII + RIF4');
+% 
+% hold on;
+% subplot(4,1,4);
+% plot(son_bruite,'r');
+% plot(s_debruiterRIF16,'g');
+% plot(s_debruitrer,'b');
+% hold off
 
 % soundsc(s_debruiterRIF4,fe);
 % pause(4);
